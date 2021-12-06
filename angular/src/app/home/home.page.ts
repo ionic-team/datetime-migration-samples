b149ce4f07bae60e681ad83bf7d025b0f6406c9c
Liam DeBeasi <liamdebeasi@icloud.com>
2021-12-06 19:31:12 +0000

    feat(angular): add Ionic 6 datetime migration

--- angular/src/app/home/home.page.ts
+++ angular/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { format, parseISO } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public formattedDate?: string;
+  public dateValue?: string;
 
   constructor() {}
+
+  formatDate() {
+    this.formattedDate = format(parseISO(this.dateValue), 'MMM d, yyyy');
+  }
 
 }
